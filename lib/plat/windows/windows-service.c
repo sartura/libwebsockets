ca233d174c8ad04cc9a5d31217742620d7e8b0c4
Andy Green <andy@warmcat.com>
2020-05-25 13:17:48 +0100

    client: allow setting overall connect timeout from vh init
    Add a member to the vh init struct allowing control of the overall
    connection wait introduced in an earlier patch.  Set it to 20s
    by default.
    
    
    The timeout_secs member controls the individual DNS result
    connect timeout and is reduced to 5s by default.

--- lib/plat/windows/windows-service.c
+++ lib/plat/windows/windows-service.c
@@ -194,13 +194,29 @@ _lws_plat_service_tsi(struct lws_context *context, int timeout_ms, int tsi)
 		pfd->revents = (short)networkevents.lNetworkEvents;
 
 		err = networkevents.iErrorCode[FD_CONNECT_BIT];
+		if (networkevents.lNetworkEvents & FD_CONNECT) {
+			lwsl_debug("%s: FD_CONNECT\n", __func__);
+			pfd->revents &= ~LWS_POLLOUT;
+			if (err && err != LWS_EALREADY &&
+			    err != LWS_EINPROGRESS && err != LWS_EWOULDBLOCK &&
+			    err != WSAEINVAL) {
+				lwsl_debug("Unable to connect errno=%d\n", err);
 
-		if ((networkevents.lNetworkEvents & FD_CONNECT) &&
-		     err && err != LWS_EALREADY &&
-		     err != LWS_EINPROGRESS && err != LWS_EWOULDBLOCK &&
-		     err != WSAEINVAL) {
-			lwsl_debug("Unable to connect errno=%d\n", err);
-			pfd->revents |= LWS_POLLHUP;
+				/*
+				 * the connection has definitively failed... but
+				 * do we have more DNS entries to try?
+				 */
+				if (wsi_from_fd(context, pfd->fd)->dns_results_next) {
+					lws_sul_schedule(context, 0, &wsi_from_fd(context, pfd->fd)->
+									sul_connect_timeout,
+							 lws_client_conn_wait_timeout, 1);
+					continue;
+                               } else
+					pfd->revents |= LWS_POLLHUP;
+			} else
+				lws_client_connect_3_connect(wsi_from_fd(context, pfd->fd),
+								NULL, NULL, LWS_CONNECT_COMPLETION_GOOD,
+								NULL);
 		}
 
 		if (pfd->revents & LWS_POLLOUT) {
